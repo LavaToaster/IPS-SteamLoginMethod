848fdf99a621c7b1c51d6acbe23feee9633f6c1a
Adam <adam@lavoaster.co.uk>
2012-05-09 18:11:28 +0100

    Added Steam Button to register page.

---
+++ Hook Files/steamDisplayButtonRegister.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author Adam Lavin (Lavoaster)
+ * @copyright 2012
+ * @license http://opensource.org/licenses/mit-license.php The MIT License
+ */
+class steamDisplayButtonRegister
+{
+    public function __construct()
+    {
+        $this->registry = ipsRegistry::instance();
+        $this->lang     = $this->registry->getClass('class_localization');
+        ipsRegistry::getClass('class_localization')->loadLanguageFile( array( 'public_steam_login' ), 'core' );
+    }
+    
+    public function getOutput()
+    {
+        $base_url = ipsRegistry::$settings['base_url'];
+        $board_url = ipsRegistry::$settings['board_url'];
+        $hash = ipsRegistry::instance()->member()->form_hash;
+        if(IPSLib::loginMethod_enabled('facebook') || IPSLib::loginMethod_enabled('twitter')){
+            $IPBHTML = <<<HTML
+<li><a href="{$base_url}app=core&amp;module=global&amp;section=login&amp;do=process&amp;use_steam=1&amp;auth_key={$hash}"><img src='{$board_url}/public/style_extra/signin/login-steam.png' alt='Steam' /></a></li>
+HTML;
+        }
+        return $IPBHTML;
+    }
+}
+?>
