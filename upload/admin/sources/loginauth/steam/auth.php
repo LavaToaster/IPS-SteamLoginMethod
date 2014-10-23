cc0c9432479eb0efbbe79a51e6118f00be9798db
Adam Lavin <adam@lavoaster.co.uk>
2014-10-23 21:27:35 +0100

    Don't check for https by checking if HTTPS is empty and replaced return_to ternary statement
    Fixes #15

--- upload/admin/sources/loginauth/steam/auth.php
+++ upload/admin/sources/loginauth/steam/auth.php
@@ -4,103 +4,95 @@
  * @copyright 2012
  * @license http://opensource.org/licenses/mit-license.php The MIT License
  */
-if ( ! defined( 'IN_IPB' ) )
-{
+if (!defined('IN_IPB')) {
     print "<h1>Incorrect access</h1>You cannot access this file directly. If you have recently upgraded, make sure you upgraded 'admin.php'.";
     exit();
 }
 
 class login_steam extends login_core implements interface_login
 {
-/**
-*       Properties passed from database entry for this method
-*       @access protected
-*       @param  array
-*/
-protected $method_config        = array();
-/**
-*       Properties passed from conf.php for this method
-*       @access protected
-*       @param  array
-*/
-protected $external_conf        = array();
+    /**
+     * @param array $method_config Properties passed from database entry for this method
+     */
+    protected $method_config = array();
+    /**
+     * @param array $external_conf Properties passed from conf.php for this method
+     */
+    protected $external_conf = array();
 
-/**
-*       Constructor
-*       @access public
-*       @param  object  ipsRegistry object
-*       @param  array   DB entry array
-*       @param  array   conf.php array
-*/
-public function __construct( ipsRegistry $registry, $method, $conf=array() )
-{
-        $this->method_config    = $method;
-        $this->external_conf    = $conf;
-        
-        require_once( IPS_ROOT_PATH . 'sources/loginauth/steam/lib/steam_openid.php' );
-                
-        parent::__construct( $registry );
-}
-
-/**
-*       Authenticate the member against your own system
-*       @access  public
-*       @param   string  Username
-*       @param   string  Email Address
-*       @param   string  Plain text password entered from log in form
-*/
-public function authenticate( $username, $email_address, $password )
-{
-    //Does the board use HTTPS For logins?
-    $board_url = ipsRegistry::$settings['logins_over_https'] ? ipsRegistry::$settings['board_url_https'] : ipsRegistry::$settings['board_url'];
-
-    //If the board uses HTTPS For logins and we are not using HTTPS get our butts onto HTTPS. Why? Because open id thinks the same website using http and https are actually different ones
-    if(ipsRegistry::$settings['logins_over_https'] and !$_SERVER['HTTPS']) $this->registry->output->silentRedirect( ipsRegistry::$settings['base_url_https']."app=core&amp;module=global&amp;section=login&amp;do=process&amp;use_steam=1&amp;auth_key=".ipsRegistry::instance()->member()->form_hash );
-
-    $steam_url = SteamSignIn::genUrl($board_url.'/interface/board/steam.php');
-    
-    // I say, Does this user be who he claims to be?
-    $steam_id = $this->request['use_steam'] ? SteamSignIn::validate() : null;
-    
-    if( !$steam_id AND $this->request['use_steam'] )
+    /**
+     * Constructor
+     *
+     * @param ipsRegistry $registry
+     * @param array $method
+     * @param array $conf
+     */
+    public function __construct(ipsRegistry $registry, $method, $conf = array())
     {
-        $this->registry->output->silentRedirect( $steam_url );
+        $this->method_config = $method;
+        $this->external_conf = $conf;
+
+        require_once(IPS_ROOT_PATH . 'sources/loginauth/steam/lib/steam_openid.php');
+
+        parent::__construct($registry);
     }
-    
-    
-    //Passport Please
-    if ( $steam_id )
+
+    /**
+     * Authenticates the steam member
+     *
+     * @param string $username Username
+     * @param string $email_address Email Address
+     * @param string $password Plain text password entered from log in form
+     *
+     * @return bool
+     */
+    public function authenticate($username, $email_address, $password)
     {
-        //We have validated your Identity!
-        $localMember = $this->DB->buildAndFetch(array('select' => 'member_id', 'from' => 'members', 'where' => "steamid='".$steam_id."'"));
-        
-        //Have you been here before?        
-        if ( $localMember['member_id'] )
-        {
-            //Welcome Back lets just log you in here
-            $this->member_data = IPSMember::load( $localMember['member_id'], 'extendedProfile,groups' );;
-            $this->return_code = 'SUCCESS';
+        //Does the board use HTTPS For logins?
+        $board_url = ipsRegistry::$settings['logins_over_https'] ? ipsRegistry::$settings['board_url_https'] : ipsRegistry::$settings['board_url'];
+
+        //If the board uses HTTPS For logins and we are not using HTTPS get our butts onto HTTPS. Why? Because open id thinks the same website using http and https are actually different ones
+        if (ipsRegistry::$settings['logins_over_https'] and $_SERVER['HTTPS'] != 'on') $this->registry->output->silentRedirect(ipsRegistry::$settings['base_url_https'] . "app=core&amp;module=global&amp;section=login&amp;do=process&amp;use_steam=1&amp;auth_key=" . ipsRegistry::instance()->member()->form_hash);
+
+        $steam_url = SteamSignIn::genUrl($board_url . '/interface/board/steam.php');
+
+        // I say, Does this user be who he claims to be?
+        $steam_id = $this->request['use_steam'] ? SteamSignIn::validate() : null;
+
+        if (!$steam_id AND $this->request['use_steam']) {
+            $this->registry->output->silentRedirect($steam_url);
         }
-        else
-        {
-            //Welcome, lets just set you up a temporary account you can fill in the details in a second
-            $email = $name = '';
-            
-            $this->member_data = $this->createLocalMember( array( 'members'            => array(
-                                                                                         'email'                    => $email,
-                                                                                         'name'                        => $name,
-                                                                                         'members_l_username'        => strtolower($name),
-                                                                                         'members_display_name'        => $name,
-                                                                                         'members_l_display_name'    => strtolower($name),
-                                                                                         'joined'                    => time(),
-                                                                                         'members_created_remote'    => 1,
-                                                                                         'steamid'                    => $steam_id,
-                                                                                        ),
-                                                                                        ) );
-            $this->return_code = 'SUCCESS';
+
+
+        //Passport Please
+        if ($steam_id) {
+            //We have validated your Identity!
+            $localMember = $this->DB->buildAndFetch(array('select' => 'member_id', 'from' => 'members', 'where' => "steamid='" . $steam_id . "'"));
+
+            //Have you been here before?
+            if ($localMember['member_id']) {
+                //Welcome Back lets just log you in here
+                $this->member_data = IPSMember::load($localMember['member_id'], 'extendedProfile,groups');;
+                $this->return_code = 'SUCCESS';
+            } else {
+                //Welcome, lets just set you up a temporary account you can fill in the details in a second
+                $email = $name = '';
+
+                $this->member_data = $this->createLocalMember(array('members' => array(
+                    'email' => $email,
+                    'name' => $name,
+                    'members_l_username' => strtolower($name),
+                    'members_display_name' => $name,
+                    'members_l_display_name' => strtolower($name),
+                    'joined' => time(),
+                    'members_created_remote' => 1,
+                    'steamid' => $steam_id,
+                ),
+                ));
+                $this->return_code = 'SUCCESS';
+            }
+            $this->request['rememberMe'] = TRUE;
+            return true;
         }
-        $this->request['rememberMe'] = TRUE;
-        return true;
     }
-}
 }
--- upload/admin/sources/loginauth/steam/lib/steam_openid.php
+++ upload/admin/sources/loginauth/steam/lib/steam_openid.php
@@ -1,45 +1,48 @@
 <?php
 /**
-*
-* @package Steam Community API
-* @copyright (c) 2010 ichimonai.com
-* @license http://opensource.org/licenses/mit-license.php The MIT License
-*
-*/
+ *
+ * @package Steam Community API
+ * @copyright (c) 2010 ichimonai.com
+ * @license http://opensource.org/licenses/mit-license.php The MIT License
+ *
+ */
 
 class SteamSignIn
 {
     const STEAM_LOGIN = 'https://steamcommunity.com/openid/login';
 
     /**
-    * Get the URL to sign into steam
-    *
-    * @param mixed returnTo URI to tell steam where to return, MUST BE THE FULL URI WITH THE PROTOCOL
-    * @param bool useAmp Use &amp; in the URL, true; or just &, false. 
-    * @return string The string to go in the URL
-    */
+     * Get the URL to sign into steam
+     *
+     * @param mixed returnTo URI to tell steam where to return, MUST BE THE FULL URI WITH THE PROTOCOL
+     * @param bool useAmp Use &amp; in the URL, true; or just &, false.
+     * @return string The string to go in the URL
+     */
     public static function genUrl($returnTo = false, $useAmp = true)
     {
-        $returnTo = (!$returnTo) ? (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'] : $returnTo;
-        
+        if (!$returnTo) {
+            $returnTo = ($_SERVER['HTTPS'] == 'on' ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'];
+        }
+
         $params = array(
             'openid.ns'            => 'http://specs.openid.net/auth/2.0',
             'openid.mode'        => 'checkid_setup',
             'openid.return_to'    => $returnTo,
-            'openid.realm'        => (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'],
+            'openid.realm'        => ($_SERVER['HTTPS'] == 'on' ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'],
             'openid.identity'    => 'http://specs.openid.net/auth/2.0/identifier_select',
             'openid.claimed_id'    => 'http://specs.openid.net/auth/2.0/identifier_select',
         );
-        
+
         $sep = ($useAmp) ? '&amp;' : '&';
+
         return self::STEAM_LOGIN . '?' . http_build_query($params, '', $sep);
     }
-    
+
     /**
-    * Validate the incoming data
-    *
-    * @return string Returns the SteamID64 if successful or empty string on failure
-    */
+     * Validate the incoming data
+     *
+     * @return string Returns the SteamID64 if successful or empty string on failure
+     */
     public static function validate()
     {
         // Start off with some basic params
@@ -49,24 +52,24 @@ class SteamSignIn
             'openid.sig'            => $_GET['openid_sig'],
             'openid.ns'                => 'http://specs.openid.net/auth/2.0',
         );
-        
+
         // Get all the params that were sent back and resend them for validation
         $signed = explode(',', $_GET['openid_signed']);
         foreach($signed as $item)
         {
             $val = $_GET['openid_' . str_replace('.', '_', $item)];
-            $params['openid.' . $item] = get_magic_quotes_gpc() ? stripslashes($val) : $val; 
+            $params['openid.' . $item] = get_magic_quotes_gpc() ? stripslashes($val) : $val;
         }
 
         // Finally, add the all important mode. 
         $params['openid.mode'] = 'check_authentication';
-      
+
         //why do we do this? cause file_get_contents to a url goes left in many server configs... IPS is sturdier
         $classToLoad    = IPSLib::loadLibrary( IPS_KERNEL_PATH . '/classFileManagement.php', 'classFileManagement' );
         $classFileManagement = new $classToLoad;
         $result = $classFileManagement->postFileContents(self::STEAM_LOGIN, $params);
         //also... i could swear i commited this fork.
-        
+
         // Validate wheather it's true and if we have a good ID
         preg_match("#^http://steamcommunity.com/openid/id/([0-9]{17,25})#", $_GET['openid_claimed_id'], $matches);
         $steamID64 = is_numeric($matches[1]) ? $matches[1] : 0;
