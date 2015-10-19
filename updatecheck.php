58b47d7fb7858c4729cfa4e547d2d9b7c45215a9
Justin Barth <madman32395@users.noreply.github.com>
2015-10-19 04:29:53 -0500

    Create updatecheck.php
    Should allow the plugin to check for updates

---
+++ updatecheck.php
@@ -0,0 +1,6 @@
+{
+    "version": "2.0.0",
+    "longversion": 20000,
+    "released": 1430265600,
+    "updateurl": "https://community.invisionpower.com/files/file/7555-sign-in-through-steam-ipb4/"
+}
